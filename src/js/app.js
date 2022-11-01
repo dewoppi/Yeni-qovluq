403395859c6d59225cba4235035a4e95bf38ab97
dewoppi <gunel.haciyeva.1987@mail.ru>
2022-11-01 10:31:50 +0400

    test comint

---
+++ src/js/app.js
@@ -0,0 +1,78 @@
+// var a= Number(prompt("enter the number"));
+// if(a%3==0 && a%5==0){
+//     console.log("eded 3e ve 5e bolunur");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var a= Number(prompt("eded daxil edin"));
+// if(a%4==0 && a%5==0) {
+//     console.log("eded 4 ve 5e bolunur");
+// }
+
+
+
+
+// var a=Number(prompt("eded yazin"));
+// if (a%6==0 && a%10==0){
+//     console.log("eded 6 ve 10a bolunur");
+// }
+
+
+// var a=Number(prompt("reqem yazin"));
+// if(a%7==0 && a%3==0){
+//     console.log("eded 7 ve 3e bolunur");
+// }
+
+// else if (a%3==0){
+//     console.log("eded yalniz 3e bolunur");
+// }
+// else if(a%7==0){
+//     console.log("eded yalniz 7ye bolunur");
+// }
+
+// var group= "hello from aksent and aksent"
+
+// console.log(group. replaceAll("aksent", "samir" ));
+
+// var sinif= "otaqda iki usaq  iki boyuk var"
+// console.log(sinif.replaceAll("iki", "uc"));
+
+// var group1=["lale", "samir", "ilkin"]
+// console.log(group1[group1.length -3]);
+
+// var person={
+//     name: "hemid",
+//     surname:"yusifli"
+// }
+// Object.freeze(person);
+// person.name="azer"
+
+// console.log(person.name);
+
+var names=["Azer", "Fuad", "Cemil"]
+names.find((e)=>e==="Azer"?console.log(true):console.log(false))
+
+
+console.log();
